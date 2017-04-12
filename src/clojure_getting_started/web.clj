d2ee3987a6422f97572226981dd8da3ff8e9d4e4
arjamizo <arjamizo@gmail.com>
2017-04-12 23:23:57 +0200

    table

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -6,10 +6,37 @@
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]))
 
+(defn row [a b c d e]
+  (apply str
+   "<tr>"
+   "<td>" a "</td>"
+   "<td>" b "</td>"
+   "<td>" c "</td>"
+   "<td>" d "</td>"
+   "<td>" e "</td>"
+   "</tr>"
+  ))
+
 (defn splash []
   {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello from Heroku"})
+   :headers {"Content-Type" "text/html"}
+   :body ; "Hello"
+   (apply str
+	  "<h1>Backup Matrix</h1>"
+	  "<table>"
+	  (apply str (repeat 4
+		  (row "<select><option>ssh-key</option>
+	       <option>login/pass</option></select>"
+		       "<input type=text value='158.1.23.55' />"
+		       "<select><option>database</option>
+	       <option>folder</option></select><input type=text />"
+		       "<input type=submit value='Force backup now'/>"
+		       "<select><option>2017-04-12 8:00</option>
+	       <option>2017-04-12 8:00</option></select><input type=submit value='Recover'/>"
+	       )
+		  ))
+   		 (row "" "" "" "" "<input type=button value='Add new'>")  "</table>")
+   })
 
 (defroutes app
   (GET "/" []
